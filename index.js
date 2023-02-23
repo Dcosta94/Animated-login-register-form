60bdc8709ebf26d52f34bc7b199666eb3dd09c88
Dcosta94 <97356630+Dcosta94@users.noreply.github.com>
2023-02-23 10:06:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const loginsec=document.querySelector('.login-section')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginsec.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginsec.classList.remove('active')
+})
